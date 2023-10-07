1925b8f179512a8b9759d3f72293fdcd8ed6babd
Murad Masimli <murad.mesimli.99@gmail.com>
2023-10-07 23:48:32 +0400

    feat:tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: '#333333',
+        primary: '#2E3192',
+        'primary-2': '#F7941D',
+        success: '#1A9447',
+        pending: '#F29339',
+        destructive: '#941A1A'
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 };
