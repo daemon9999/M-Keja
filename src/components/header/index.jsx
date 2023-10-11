d340e6823d5f378fa7862debf7a614d0fa704f29
Murad Masimli <murad.mesimli.99@gmail.com>
2023-10-11 09:58:31 +0400

    feat:logo config

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,12 +7,9 @@ export default function Header() {
       <div className="max-w-[1280px] mx-auto  h-full flex items-center  justify-between">
         {/* LEFT SIDE */}
         <div className="flex gap-x-10 items-center">
-          <Link
-            to={'/'}
-            className="bg-primary-2  h-10 w-[180px]  flex items-center justify-center mr-10"
-          >
-            <h4 className="text-white font-semibold text-xl">Logo</h4>
-          </Link> 
+          <Link to={'/'} className='h-[86px] w-auto'>
+            <img src="/images/logo/header-logo.png" className='h-full w-auto' alt="AFRI Logo" />
+          </Link>
           <Link to={'/buy'}>
             <span className="px-2 py-1 rounded-full hover:bg-neutral/10 transition-colors">
               Buy
